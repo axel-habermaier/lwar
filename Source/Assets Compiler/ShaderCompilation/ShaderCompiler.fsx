b896fc5c47a745a5a826348d850bc7dbf42aabef
Axel <mail@axel-habermaier.de>
2013-03-26 19:39:12 +0100

    Find effect classes via reflection

--- Source/Assets Compiler/ShaderCompilation/ShaderCompiler.fsx
+++ Source/Assets Compiler/ShaderCompilation/ShaderCompiler.fsx
@@ -1,4 +1,55 @@
-﻿printfn "Command line arguments: "
+﻿open System
+open System.Reflection
 
-for arg in fsi.CommandLineArgs do
-    printfn "%s" arg
+//====================================================================================================================
+// Shader metamodel
+//====================================================================================================================
+type Semantics =
+    | Position
+    | TexCoords of int
+    | Normal
+    | Color of int
+
+type Parameter = {
+    Name : string;
+    Semantics : Semantics;
+}
+
+type ShaderType = 
+    | VertexShader
+    | FragmentShader
+
+type Shader = {
+    Type : ShaderType;
+    Name : string;
+    Inputs : Parameter list;
+    Outputs : Parameter list;
+}
+
+//====================================================================================================================
+// Reflection
+//====================================================================================================================
+let reflectShaders (assembly : Assembly) =
+    let isEffect (t : Type) =
+        t.GetCustomAttributes() 
+            |> Seq.map (fun a -> a.ToString())
+            |> Seq.exists (fun n -> n = "Pegasus.AssetsCompiler.ShaderCompilation.EffectAttribute")
+
+    let effects =
+        assembly.GetTypes() |> 
+            Seq.filter (fun t -> isEffect t)
+
+    for e in effects do
+        printfn "%s" e.FullName
+
+//====================================================================================================================
+// Execute compiler
+//====================================================================================================================
+if fsi.CommandLineArgs.Length < 3 then
+    printfn "Invalid shader compiler invocation: [assembly name] [C# file 1] [C# file 2] ..."
+
+let assetAssembly = fsi.CommandLineArgs.[1]
+let csharpFiles = fsi.CommandLineArgs.[2..]
+
+let assembly = Assembly.LoadFile(assetAssembly)
+reflectShaders(assembly)
