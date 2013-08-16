c0e65fe9bc6d5cb9cb2eca26e54c851461fd5ac2
AxelHabermaier <mail@axel-habermaier.de>
2013-08-16 14:26:29 +0200

    Added comment

--- Source/lwar/Network/ServerConnection.cs
+++ Source/lwar/Network/ServerConnection.cs
@@ -196,18 +196,21 @@ namespace Lwar.Network
 				if (!buffer.TryRead(out messages, MessageDeserializer))
 					continue;
 
-				var allowReliableDelivery = messages[0].Type.IsReliable() && deliveryManager.AllowReliableDelivery(messages[0].SequenceNumber);
-				var allowUnreliableDelivery = messages[0].Type.IsUnreliable() && deliveryManager.AllowUnreliableDelivery(messages[0].SequenceNumber);
+				// There are two cases here:
+				// - messages.Count == 1: This happens when a single protocol message results in a single Message instance
+				// - messages.Count  > 1: This happens when a single protocol message is split into several Message instances
+				// In either case, we can check if delivery is allowed for all generated Message instances, as they all
+				// have the same sequence number and type.
+				var message = messages[0];
+				if (message.Type.IsReliable() && !deliveryManager.AllowReliableDelivery(message.SequenceNumber))
+					continue;
+
+				if (message.Type.IsUnreliable() && !deliveryManager.AllowUnreliableDelivery(message.SequenceNumber))
+					continue;
 
 				for (var i = 0; i < messages.Count; ++i)
 				{
-					var message = messages[i];
-
-					if (message.Type.IsReliable() && !allowReliableDelivery)
-						continue;
-
-					if (message.Type.IsUnreliable() && !allowUnreliableDelivery)
-						continue;
+					message = messages[i];
 
 					if (message.Type.IsUnreliable())
 						message.Timestamp = header.Value.Timestamp;
