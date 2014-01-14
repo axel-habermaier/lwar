071e6afc08462105e8e01c0a5aa36c04787d3daa
AxelHabermaier <mail@axel-habermaier.de>
2014-01-14 15:54:46 +0100

    UI solution

--- Source/UI/lwar UI/Properties/AssemblyInfo.cs
+++ Source/UI/lwar UI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pegasus UI")]
+[assembly: AssemblyTitle("lwar UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pegasus UI")]
+[assembly: AssemblyProduct("lwar UI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
