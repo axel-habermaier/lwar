d27cd652213124689066b1e99ced1a32345ee970
Axel777 <expand-able@web.de>
2014-01-14 17:18:46 +0100

    Asset compiler refactoring undone

---
+++ Source/lwar Assets/EntityTemplates/Compilation/EntityTemplate.cs
@@ -0,0 +1,46 @@
+﻿using Pegasus.AssetsCompiler.Assets.Attributes;
+
+[assembly: Ignore("EntityTemplates/Compilation/EntityTemplate.cs")]
+
+namespace Lwar.Assets.EntityTemplates.Compilation
+{
+	using System;
+	using Pegasus.Math;
+
+	/// <summary>
+	///   Represents an entity template that is shared between the client and the server.
+	/// </summary>
+	public class EntityTemplate
+	{
+		public Vector2 Acceleration;
+		public string Act;
+		public string Collide;
+		public string CubeMap;
+		public Vector2 Decelaration;
+		public float Energy;
+		public string Format;
+		public float Health;
+		public int Interval;
+		public float Length;
+		public float Mass;
+		public Func<EntityTemplate, string> Model;
+
+		/// <summary>
+		///   The name of the template. This field is set by the template compiler.
+		/// </summary>
+		public string Name;
+
+		public float Radius;
+		public float Rotation;
+		public float Shield;
+		public string Texture;
+
+		/// <summary>
+		///   Initializes a new instance.
+		/// </summary>
+		public EntityTemplate()
+		{
+			Model = _ => null;
+		}
+	}
+}
