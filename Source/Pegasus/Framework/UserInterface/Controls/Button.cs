17473e8961509c9e8ccf4060646b4dcee3125aa8
Axel Habermaier <mail@axel-habermaier.de>
2014-08-07 17:53:24 +0200

    Cleanup

--- Source/Pegasus/Framework/UserInterface/Controls/Button.cs
+++ Source/Pegasus/Framework/UserInterface/Controls/Button.cs
@@ -9,7 +9,7 @@
 	public class Button : ContentControl
 	{
 		/// <summary>
-		///     Raised when the button has been clicked or the enter key has been pressed while the button has the keyboard focus.
+		///     Raised when the button has been clicked.
 		/// </summary>
 		public static readonly RoutedEvent<RoutedEventArgs> ClickEvent = new RoutedEvent<RoutedEventArgs>(RoutingStrategy.Bubble);
 
@@ -18,41 +18,16 @@
 		/// </summary>
 		static Button()
 		{
-			KeyDownEvent.Raised += OnKeyDown;
 			MouseLeftButtonUpEvent.Raised += OnLeftMouseButtonUp;
 		}
 
 		/// <summary>
-		///     Initializes a new instance.
-		/// </summary>
-		public Button()
-		{
-			Focusable = true;
-		}
-
-		/// <summary>
-		///     Raised when the button has been clicked or the enter key has been pressed while the button has the keyboard focus.
+		///     Raised when the button has been clicked.
 		/// </summary>
 		public event RoutedEventHandler<RoutedEventArgs> Click
 		{
 			add { AddHandler(ClickEvent, value); }
 			remove { RemoveHandler(ClickEvent, value); }
-		}
-
-		/// <summary>
-		///     Checks whether the enter key has been pressed and if so, raises the Click event and sets the mouse event to handled.
-		/// </summary>
-		private static void OnKeyDown(object sender, KeyEventArgs e)
-		{
-			var button = sender as Button;
-			if (button == null)
-				return;
-
-			if (e.Key != Key.Return && e.Key != Key.NumpadEnter)
-				return;
-
-			e.Handled = true;
-			button.RaiseEvent(ClickEvent, RoutedEventArgs.Default);
 		}
 
 		/// <summary>
