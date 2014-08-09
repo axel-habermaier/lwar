3d486ce75c8555beb45626b517a240e7449206bf
Axel Habermaier <mail@axel-habermaier.de>
2014-08-09 19:45:03 +0200

    AreaPanel WPF layouting

--- Source/User Interface/Pegasus UI/UserInterface/Controls/AreaPanel.cs
+++ Source/User Interface/Pegasus UI/UserInterface/Controls/AreaPanel.cs
@@ -1,9 +1,26 @@
 ﻿namespace Pegasus.Framework.UserInterface.Controls
 {
 	using System;
+	using System.Windows;
 	using System.Windows.Controls;
 
 	public class AreaPanel : Panel
 	{
+
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			foreach (UIElement child in Children)
+				child.Measure(availableSize);
+
+			return availableSize;
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			foreach (UIElement child in Children)
+				child.Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
+
+			return finalSize;
+		}
 	}
 }
