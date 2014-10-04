5347bea587b6c1d3b3fbf5752ba22d8eaab9e3fe
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2014-10-04 18:23:15 +0200

    format string check for msvc. hope it works.

--- Source/Lwar/Server/attributes.h
+++ Source/Lwar/Server/attributes.h
@@ -2,10 +2,8 @@
 
 #ifdef _MSC_VER
 	#define NORETURN __declspec(noreturn)
-    #define FORMAT
 #else
 	#define NORETURN __attribute__ ((noreturn))
-	#define FORMAT   __attribute__ ((format(printf,1,2)))
 #endif
 
 #endif
--- Source/Lwar/Server/log.h
+++ Source/Lwar/Server/log.h
@@ -3,10 +3,19 @@
 
 #include "attributes.h"
 
-FORMAT NORETURN void log_die(const char* message, ...);
-FORMAT void log_error(const char* message, ...);
-FORMAT void log_warn(const char* message, ...);
-FORMAT void log_info(const char* message, ...);
-FORMAT void log_debug(const char* message, ...);
+#ifdef _MSC_VER
+    NORETURN void log_die(_Printf_format_string_ const char* message, ...);
+    void log_error(_Printf_format_string_ const char* message, ...);
+    void log_warn(_Printf_format_string_ const char* message, ...);
+    void log_info(_Printf_format_string_ const char* message, ...);
+    void log_debug(_Printf_format_string_ const char* message, ...);
+#else
+	#define FORMAT   __attribute__ ((format(printf,1,2)))
+    FORMAT NORETURN void log_die(const char* message, ...);
+    FORMAT void log_error(const char* message, ...);
+    FORMAT void log_warn(const char* message, ...);
+    FORMAT void log_info(const char* message, ...);
+    FORMAT void log_debug(const char* message, ...);
+#endif
 
 #endif
