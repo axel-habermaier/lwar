f52eb31696f0082d859843998dabd696c58c312b
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2014-10-04 18:15:20 +0200

    assert now depends on -DDEBUG.

--- Source/Lwar/Server/debug.h
+++ Source/Lwar/Server/debug.h
@@ -5,8 +5,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifdef DEBUG
 #define assert(test) \
     debug_assert((test), #test, __FILE__, __LINE__)
+#else
+#define assert(test)
+#endif
 
 void debug_assert(bool test, const char *what, const char *file, size_t line);
 
