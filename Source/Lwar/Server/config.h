962fd182444dba8827dc40c844cb9256d5039923
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2014-10-06 21:54:37 +0200

    moved UPDATE_INTERVAL to config.h.

--- Source/Lwar/Server/config.h
+++ Source/Lwar/Server/config.h
@@ -10,6 +10,7 @@ enum {
 	MULTICAST_PORT      = SERVER_PORT + 1,
 	MULTICAST_TTL		= 1,
 
+    UPDATE_INTERVAL     = 30        /*ms*/, /* only used if server_update is called with force == false */
 	DISCOVERY_INTERVAL  =  5 * 1000 /*ms*/, /* send discovery every 5 seconds   */
     TIMEOUT_INTERVAL    = 15 * 1000 /*ms*/, /* drop connection after 15 seconds */
     RETRANSMIT_INTERVAL =       100 /*ms*/,
