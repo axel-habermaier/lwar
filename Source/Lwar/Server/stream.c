d55a14ba099076d31d21d938828bf213f32d01f0
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2014-10-07 21:53:58 +0200

    supposedly working implementation.

--- Source/Lwar/Server/stream.c
+++ Source/Lwar/Server/stream.c
@@ -2,12 +2,13 @@
 
 #include "stream.h"
 
+#include "debug.h"
 #include "message.h"
-#include "unpack.h"
-#include "pack.h"
 #include "packet.h"
+#include "pack.h"
 #include "performance.h"
 #include "server.h"
+#include "unpack.h"
 
 /*
 static struct {
@@ -91,25 +92,70 @@ bool stream_recv(cr_t *state, Header *h, Message *m) {
     cr_return(state, false);
 }
 
+static void packet_init_send_header(Packet *p, Header *h) {
+    packet_init_send(p, &h->adr);
+    packet_put(p, header_pack, h);
+}
+
+static void packet_flush(Packet *p) {
+    if(packet_hasdata(p))
+        packet_send(p);
+}
+
+static void send_message(Packet *p, Header *h, Message *m) {
+    while(!packet_put(p, message_pack, m)) {
+        packet_send(p);
+        packet_init_send_header(p, h);
+    }
+}
+
+static void send_update_messate(Packet *p, Header *h, Message *m) {
+    Entity *e;
+    Format *f = m->update.f;
+    size_t k = 0;
+    size_t n = f->n;
+
+    updates_foreach(f,e) {
+        if(e->dead) { n --; continue; }
+    retry:
+        if(!k) {
+            k = min(n, packet_update_n(p,f->len));
+            if(k) {
+                m->update.n = k;
+                packet_put(p, message_pack, m);
+            } else {
+                packet_send(p);
+                packet_init_send_header(p, h);
+            }
+            goto retry;
+        } else {
+            packet_put(p, f->pack, e);
+            k --;
+            n --;
+        }
+    }
+    assert(k == 0);
+    assert(n == 0);
+}
+
 void stream_send(cr_t *state, Header *h, Message *m) {
     static Packet p;
 
     cr_begin(state);
 
-again:
-    packet_init_send(&p, &h->adr);
-    packet_put(&p, header_pack, h);
+    packet_init_send_header(&p, h);
 
     while(m) {
-        if(!packet_put(&p, message_pack, m)) {
-            packet_send(&p);
-            goto again;
+        if(is_update(m)) {
+            send_update_messate(&p, h, m);
+        } else {
+            send_message(&p, h, m);
         }
+
         cr_pause(state);
     }
-
-    if(packet_hasdata(&p))
-        packet_send(&p);
+    
+    packet_flush(&p);
 
     cr_end(state);
 }
