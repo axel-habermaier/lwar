3e03216d60c91d79be38004cc2df8609db3bf1be
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2014-10-07 18:44:53 +0200

    added is_updated.

--- Source/Lwar/Server/message.h
+++ Source/Lwar/Server/message.h
@@ -12,6 +12,7 @@ typedef enum   MessageType MessageType;
 typedef enum   RejectReason RejectReason;
 
 bool is_reliable(Message *m);
+bool is_update(Message *m);
 
 void message_add(Message *m, Entity *e);
 void message_collision(Message *m, Collision *c);
@@ -25,47 +26,47 @@ void message_synced(Message *m);
 void message_update(Message *m, MessageType type, size_t n); /* TODO: use data structure, e.g. Format. */
 
 enum MessageType {
-    MESSAGE_CONNECT			  =   1,
-	/* */
-    MESSAGE_JOIN			  =   3,
-    MESSAGE_LEAVE			  =   4,
-    MESSAGE_CHAT			  =   5,
-    MESSAGE_ADD				  =   6,
-    MESSAGE_REMOVE			  =   7,
-    MESSAGE_SELECTION		  =   8,
-    MESSAGE_NAME			  =   9,
-    MESSAGE_SYNCED			  =  10,
-	MESSAGE_KILL			  =  11,
-
-    MESSAGE_STATS			  = 101,
+    MESSAGE_CONNECT         =   1,
     /* */
-    MESSAGE_INPUT			  = 103,
-    /* */			
-    MESSAGE_COLLISION		  = 105,
-	MESSAGE_DISCONNECT		  = 106,
-	MESSAGE_REJECT			  = 107,
+    MESSAGE_JOIN            =   3,
+    MESSAGE_LEAVE           =   4,
+    MESSAGE_CHAT            =   5,
+    MESSAGE_ADD             =   6,
+    MESSAGE_REMOVE          =   7,
+    MESSAGE_SELECTION       =   8,
+    MESSAGE_NAME            =   9,
+    MESSAGE_SYNCED          =  10,
+    MESSAGE_KILL            =  11,
 
-    MESSAGE_UPDATE			  = 110,
-    MESSAGE_UPDATE_POS		  = 111,
-    MESSAGE_UPDATE_RAY		  = 112,
-    MESSAGE_UPDATE_CIRCLE	  = 113,
-	MESSAGE_UPDATE_SHIP		  = 114,
+    MESSAGE_STATS           = 101,
+    /* */
+    MESSAGE_INPUT           = 103,
+    /* */            
+    MESSAGE_COLLISION       = 105,
+    MESSAGE_DISCONNECT      = 106,
+    MESSAGE_REJECT          = 107,
 
+    /* Note: adapt is_update in message.c after adding more updates */
+    MESSAGE_UPDATE          = 110,
+    MESSAGE_UPDATE_POS      = 111,
+    MESSAGE_UPDATE_RAY      = 112,
+    MESSAGE_UPDATE_CIRCLE   = 113,
+    MESSAGE_UPDATE_SHIP     = 114,
 };
 
 enum {
-	MESSAGE_DISCOVERY		  = 200,
+    MESSAGE_DISCOVERY       = 200,
 };
 
 enum LeaveReason {
-	LEAVE_QUIT			= 1,
-	LEAVE_DROPPED		= 2,
-	LEAVE_MISBEHAVED	= 3,
+    LEAVE_QUIT              = 1,
+    LEAVE_DROPPED           = 2,
+    LEAVE_MISBEHAVED        = 3,
 };
 
 enum RejectReason {
-	REJECT_FULL				= 1,
-	REJECT_VERSION_MISMATCH = 2,
+    REJECT_FULL             = 1,
+    REJECT_VERSION_MISMATCH = 2,
 };
 
 enum HeaderState {
@@ -94,19 +95,19 @@ struct Message {
     size_t seqno;
 
     union {
-		 struct {
-			uint8_t rev;
-			Str nick;
+         struct {
+            uint8_t rev;
+            Str nick;
         } connect;
 
         struct {
             Id player_id;
-			Str nick;
+            Str nick;
         } join;
 
         struct {
             Id player_id;
-			LeaveReason reason;
+            LeaveReason reason;
         } leave;
 
         struct {
@@ -138,14 +139,14 @@ struct Message {
             Str nick;
         } name;
 
-		struct {
-			Id killer_id;
-			Id victim_id;
-		} kill;
+        struct {
+            Id killer_id;
+            Id victim_id;
+        } kill;
 
-		struct {
-			RejectReason reason;
-		} reject;
+        struct {
+            RejectReason reason;
+        } reject;
 
         struct {
             uint8_t n;
@@ -169,15 +170,15 @@ struct Message {
             int16_t  aim_y;
         } input;
 
-		struct {
-			uint8_t n;
-			struct {
-				Id player_id;
-				uint16_t kills;
-				uint16_t deaths;
-				uint16_t ping;
-			} info[MAX_CLIENTS];
-		} stats;
+        struct {
+            uint8_t n;
+            struct {
+                Id player_id;
+                uint16_t kills;
+                uint16_t deaths;
+                uint16_t ping;
+            } info[MAX_CLIENTS];
+        } stats;
 
         struct {
             Id entity_id[2];
@@ -185,11 +186,11 @@ struct Message {
             int16_t y;
         } collision;
 
-		struct {
-			uint32_t app_id;
-			uint8_t  rev;
-			uint16_t port;
-		} discovery;
+        struct {
+            uint32_t app_id;
+            uint8_t  rev;
+            uint16_t port;
+        } discovery;
     };
 };
 
